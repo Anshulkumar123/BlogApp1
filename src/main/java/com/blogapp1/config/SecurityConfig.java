83b6bccf12a95eecabd0180cd185988cd576f95f
Anshulkumar123 <168398368+Anshulkumar123@users.noreply.github.com>
2024-07-15 16:05:50 +0530

    This is today work

--- src/main/java/com/blogapp1/config/SecurityConfig.java
+++ src/main/java/com/blogapp1/config/SecurityConfig.java
@@ -1,18 +1,35 @@
 package com.blogapp1.config;
 
+import com.blogapp1.security.CustomUserDetailServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private CustomUserDetailServices customUserDetailServices;
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST, "api/auth/**").permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailServices).passwordEncoder(getPasswordEncoder());
     }
 }
